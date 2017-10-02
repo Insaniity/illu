73306a7ae8a5093b6da92708fc53d4474e7ab5af
Insaniity <iiyolo@icloud.com>
2017-10-02 23:01:51 +0100

    z

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-bot.on("ready", () => {
-   	  bot.user.setStatus("dnd"); //dnd , online , ldle
-      bot.user.setGame("im special c;");
-      console.log("Running as user "+bot.user.username+"#"+bot.user.discriminator+".");
-			bot.user.setAvatar('./icon.png')
+client.on("ready", () => {
+   	  client.user.setStatus("dnd"); //dnd , online , ldle
+      client.user.setGame("im special c;");
+      console.log("Running as user "+client.user.username+"#"+client.user.discriminator+".");
+			client.user.setAvatar('./icon.png')
   			.then(user => console.log(`New avatar set!`))
   			.catch(console.error);
 });
