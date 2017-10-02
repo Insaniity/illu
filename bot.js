9480412c2e7fec9578871b837e73f8997f6d49a8
Insaniity <iiyolo@icloud.com>
2017-10-02 23:03:19 +0100

    x

--- bot.js
+++ bot.js
@@ -5,14 +5,25 @@ client.on("ready", () => {
    	  client.user.setStatus("dnd"); //dnd , online , ldle
       client.user.setGame("im special c;");
       console.log("Running as user "+client.user.username+"#"+client.user.discriminator+".");
-			client.user.setAvatar('./icon.png')
-  			.then(user => console.log(`New avatar set!`))
-  			.catch(console.error);
+//			client.user.setAvatar('./icon.png')
+// 			.then(user => console.log(`New avatar set!`))
+//			.catch(console.error);
 });
 
 
 
 client.on('message', message => {
+
+let talkedRecently = new Set();
+if (talkedRecently.has(<Message>.author.id)) {
+  return;
+}
+talkedRecently.add(<Message>.author.id);
+setTimeout(() => {
+  talkedRecently.delete(<Message>.author.id);
+}, 2500);
+
+
     if (message.content === 'ping') {
     	message.channel.send('pong');
   	}
