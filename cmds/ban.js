70216371bc3b307eed387e0134c54e60a6db55cc
Insaniity <iiyolo@icloud.com>
2017-10-03 19:59:28 +0100

    b

---
+++ cmds/ban.js
@@ -0,0 +1,25 @@
+module.exports.run = (client, message, args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do not have `BAN_MEMBERS`.");
+    if (!message.mentions.users.first()) return message.channel.send("Mention a user or multiple users to ban them.")
+    let ment = message.mentions.users;
+    let text = []
+    ment.forEach(m => {
+
+        if (!message.guild.member(m).bannable) {
+            message.channel.send("Something went wrong when banning: "+m.username);
+        } else {
+            message.guild.ban(message.guild.member(m)).then(() => {
+                text.push(m.username)
+            }).catch(err => message.channel.send("Something went wrong when banning: "+m.username))
+        }
+    });
+    setTimeout(function() {
+        if (text.length === 0) return;
+        message.channel.send(text.join(", ")+" has been banned.", {split:true});
+        console.log(message.guild.name+" Banned "+text.join(", "))
+    }, 1000);
+}
+
+module.exports.help = {
+	name: "ban"
+}
