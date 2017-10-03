8c90d4b5962418b4cfc3b54dabb6c8bbe12aff26
Insaniity <iiyolo@icloud.com>
2017-10-03 19:29:49 +0100

    nodelydodely
    lol

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment')
+const fs = require("fs");
 
 client.on("ready", () => {
    	  client.user.setStatus("dnd"); //dnd , online , ldle
@@ -75,6 +76,24 @@ client.on('guildBanAdd', (guild, user) => {
 client.on('guildBanRemove', (guild, user) => {
     exports.fire(`**User unbanned -> \`${user.tag}\`**`, guild)
 })
+
+fs.readdir("./cmds/", (err, files) => {
+	if(err) console.error(err);
+
+	let jsfiles = files.filter(f => f.split(".").pop() === "js");
+	if(jsfiles.length <= 0) {
+		console.log("No commands to load!");
+		return;
+	}
+
+	console.log(`Loading ${jsfiles.length} commands!`);
+
+	jsfiles.forEach((f, i) => {
+		let props = require(`./cmds/${f}`);
+		console.log(`${i + 1}: ${f} loaded!`);
+		bot.commands.set(props.help.name, props);
+	});
+});
 
 client.on('message', message => {
 
