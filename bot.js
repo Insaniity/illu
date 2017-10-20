9b3a4cf3f287a526ae5446056407a7a120a2d070
Insaniity <iiyolo@icloud.com>
2017-10-20 17:49:43 +0100

    aa
    aa

--- bot.js
+++ bot.js
@@ -113,22 +113,23 @@ client.on('guildBanRemove', (guild, user) => {
 })
 
 fs.readdir("./cmds/", (err, files) => {
-	if(err) console.error(err);
+    if(err) console.error(err);
 
-	let jsfiles = files.filter(f => f.split(".").pop() === "js");
-	if(jsfiles.length <= 0) {
-		console.log("No commands to load!");
-		return;
-	}
+    let jsfiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsfiles.length <= 0) {
+        console.log("No commands to load!");
+        return;
+    }
 
-	console.log(`Loading ${jsfiles.length} commands!`);
+    console.log(`Loading ${jsfiles.length} commands!`);
 
-	jsfiles.forEach((f, i) => {
-		let props = require(`./cmds/${f}`);
-		console.log(`${i + 1}: ${f} loaded!`);
-		bot.commands.set(props.help.name, props);
-	});
+    jsfiles.forEach((f, i) => {
+        let props = require(`./cmds/${f}`);
+        console.log(`${i + 1}: ${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
 });
+
 
 
 process.on('unhandledRejection', error => {
