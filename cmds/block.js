d61ee3848175c8b5268d4de468375432f116007f
Insaniity <iiyolo@icloud.com>
2017-11-11 12:56:13 +0000

    a
    a

--- cmds/block.js
+++ cmds/block.js
@@ -1,20 +1,3 @@
-    if (message.content.toLowerCase().startsWith(prefix + "blacklist")) {
-        if (message.member.hasPermission('ADMINISTRATOR')) {
-            if (!file.blockedIDs[args[0]]) {
-                file.blockedIDs[args[0]] = {
-                    blocked: "true"
-                };
-                fs.writeFileSync("./src/config.json", JSON.stringify(file));
-                message.channel.send("Added `" + message.content.substr(7) + "` to the blacklist.");
-            } else {
-                message.channel.send("ID is already blacklisted.");
-            }
-
-        } else {
-            return message.channel.send("Missing Permissions");
-        }
-    }
-
 const fs = module.require("fs");
 const kryblocked = require("../krystal/blocked.json");
 const blocked = kryblocked.blockedIDs;
